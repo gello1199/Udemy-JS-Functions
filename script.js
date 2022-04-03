24cb090f1f358ef90fcaf86a73c4f17607e50dd5
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-04-03 13:51:17 -0400

    Functions coding challenge 1 complete

--- script.js
+++ script.js
@@ -1,107 +1,178 @@
 'use strict';
 
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+
+const poll = {
+    question: 'What is your favorite programming language?',
+    options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+    answers: new Array(4).fill(0),
+
+    registerNewAnswer() {
+        // Get answer   
+        const answer = Number(
+            prompt(
+                `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+            )
+        )
+            console.log(answer)
+        // Register answer 
+        typeof answer === 'number' && answer < this.answers.length && this.answers[answer]++
+
+        this.displayResults()
+        this.displayResults('string')
+    },
+    displayResults(type = 'array') {
+        if(type === 'array') {
+            console.log(this.answers)
+        } else if (type === 'string') {
+            // "Poll results are 13, 2, 4, 1" 
+            console.log(`Poll results are ${this.answers.join(', ')}`)
+        }
+    }
+}
+// poll.registerNewAnswer()
+
+document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll))
+
+poll.displayResults.call({answers: [5, 2, 3]}, 'string')
+poll.displayResults.call({answers: [1, 5, 3, 9, 6, 1]}, 'string')
+poll.displayResults.call({answers: [1, 5, 3, 9, 6, 1]})
+
+// BONUS TEST DATA 1: [5, 2, 3]
+// BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+
 
 // The call and apply Methods
 
-const lufthansa = {
-    airline: 'Lufthansa',
-    iataCode: 'LH',
-    bookings: [],
-    // book: function() {}
-    book(flightNum, name) {
-        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`)
-        this.bookings.push({flight: `${this.iataCode}${flightNum}`, name})
-    }
-}
+// const lufthansa = {
+//     airline: 'Lufthansa',
+//     iataCode: 'LH',
+//     bookings: [],
+//     // book: function() {}
+//     book(flightNum, name) {
+//         console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`)
+//         this.bookings.push({flight: `${this.iataCode}${flightNum}`, name})
+//     }
+// }
 
-lufthansa.book(239, 'Jonas Schmedtmann')
-lufthansa.book(635, 'John Smith')
-console.log(lufthansa)
+// lufthansa.book(239, 'Jonas Schmedtmann')
+// lufthansa.book(635, 'John Smith')
+// console.log(lufthansa)
 
-const eurowings = {
-    airline: 'Eurowings',
-    iataCode: 'EW',
-    bookings: [],
-}
+// const eurowings = {
+//     airline: 'Eurowings',
+//     iataCode: 'EW',
+//     bookings: [],
+// }
 
-const book = lufthansa.book
+// const book = lufthansa.book
 
-// Does not work
-// book(23, 'Sarah Williams')
+// // Does not work
+// // book(23, 'Sarah Williams')
 
-// Call method
-book.call(eurowings, 23, 'Sarah Williams')
-console.log(eurowings)
+// // Call method
+// book.call(eurowings, 23, 'Sarah Williams')
+// console.log(eurowings)
 
-book.call(lufthansa, 239, 'Mary Cooper')
-console.log(lufthansa)
+// book.call(lufthansa, 239, 'Mary Cooper')
+// console.log(lufthansa)
 
-const swiss = {
-    airline: 'Swiss Air Lines',
-    iataCode: 'LX',
-    bookings: []
-}
+// const swiss = {
+//     airline: 'Swiss Air Lines',
+//     iataCode: 'LX',
+//     bookings: []
+// }
 
-book.call(swiss, 583, 'Mary Cooper')
+// book.call(swiss, 583, 'Mary Cooper')
+// // console.log(swiss)
+
+// // Apply Method
+// const flightData = [583, 'George Cooper']
+// // book.apply(swiss, flightData)
+// // the spread operator below is doing the same as apply. Spread is more widely used now
+// book.call(swiss, ...flightData)
 // console.log(swiss)
 
-// Apply Method
-const flightData = [583, 'George Cooper']
-// book.apply(swiss, flightData)
-// the spread operator below is doing the same as apply. Spread is more widely used now
-book.call(swiss, ...flightData)
-console.log(swiss)
+// // The bind Method
+// // book.call(eurowings, 23, 'Sarah Williams')
 
-// The bind Method
-// book.call(eurowings, 23, 'Sarah Williams')
+// // Presets airline
+// const bookEW = book.bind(eurowings)
+// const bookLH = book.bind(lufthansa)
+// const bookLX = book.bind(swiss)
 
-// Presets airline
-const bookEW = book.bind(eurowings)
-const bookLH = book.bind(lufthansa)
-const bookLX = book.bind(swiss)
+// bookEW(23, 'Steven Williams')
 
-bookEW(23, 'Steven Williams')
-
-// presets flight number
-const bookEW23 = book.bind(eurowings, 23)
-bookEW23('Jonas Schmedtmann')
-bookEW23('Martha Cooper')
+// // presets flight number
+// const bookEW23 = book.bind(eurowings, 23)
+// bookEW23('Jonas Schmedtmann')
+// bookEW23('Martha Cooper')
 
 
-// With Event Listeners
-lufthansa.planes = 300
-lufthansa.buyPlane = function() {
-    console.log(this)
+// // With Event Listeners
+// lufthansa.planes = 300
+// lufthansa.buyPlane = function() {
+//     console.log(this)
 
-    this.planes++
-    console.log(this.planes)
-}
-// lufthansa.buyPlane()
+//     this.planes++
+//     console.log(this.planes)
+// }
+// // lufthansa.buyPlane()
 
-document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa))
+// document.querySelector('.buy').addEventListener('click', lufthansa.buyPlane.bind(lufthansa))
 
-// Partial Application
+// // Partial Application
 
-const addTax = (rate, value) => value + value * rate
-console.log(addTax(0.1, 200))
+// const addTax = (rate, value) => value + value * rate
+// console.log(addTax(0.1, 200))
 
-const addVAT = addTax.bind(null, 0.23)
-// addVAT = value => value + value * 0.23
+// const addVAT = addTax.bind(null, 0.23)
+// // addVAT = value => value + value * 0.23
 
-console.log(addVAT(100))
-console.log(addVAT(23))
+// console.log(addVAT(100))
+// console.log(addVAT(23))
 
-// Challenge
-const addTaxRate = function(rate) {
-    return function(value) {
-        return value + value * rate
-    }
-}
-const addVAT2 = addTaxRate(0.23)
-console.log(addVAT2(100))
-console.log(addVAT2(23))
-
-
+// // Challenge
+// const addTaxRate = function(rate) {
+//     return function(value) {
+//         return value + value * rate
+//     }
+// }
+// const addVAT2 = addTaxRate(0.23)
+// console.log(addVAT2(100))
+// console.log(addVAT2(23))
 
 // Functions Returning Functions
 
