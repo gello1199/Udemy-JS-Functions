f4892d89ed0239b29f0b4c0355c5b7e44033633f
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-04-03 10:32:05 -0400

    Functions returning functions notes

--- script.js
+++ script.js
@@ -1,34 +1,54 @@
 'use strict';
 
+// Functions Returning Functions
+
+const greet = function(greeting) {
+    return function(name) {
+        console.log(`${greeting} ${name}`)
+    }
+}
+
+const greeterHey = greet('Hey')
+greeterHey('Jonas')
+greeterHey('Steven')
+
+greet('Hello')('Jonas')
+
+// Challenge with arrow function
+const greetArr = greeting => name => 
+console.log(`${greeting} ${name}`)
+
+greetArr('Hi there')('Joe')
+
 // Functions Accepting Callback Functions
 
-const oneWord = function(str) {
-    return str.replace(/ /g, '').toLowerCase()
-}
+// const oneWord = function(str) {
+//     return str.replace(/ /g, '').toLowerCase()
+// }
 
-const upperFirstWord = function(str) {
-    const [first, ...others] = str.split(' ')
-    return [first.toUpperCase(), ...others].join(' ')
-}
+// const upperFirstWord = function(str) {
+//     const [first, ...others] = str.split(' ')
+//     return [first.toUpperCase(), ...others].join(' ')
+// }
 
-// Higher-order Function
-const transformer = function(str, fn) {
-    console.log(`Original string: ${str}`)
-    console.log(`Transformed string: ${fn(str)}`)
+// // Higher-order Function
+// const transformer = function(str, fn) {
+//     console.log(`Original string: ${str}`)
+//     console.log(`Transformed string: ${fn(str)}`)
 
-    console.log(`Transformed by: ${fn.name}`)
-} 
+//     console.log(`Transformed by: ${fn.name}`)
+// } 
 
-transformer('JavaScript is the best', upperFirstWord)
-transformer('JavaScript is the best', oneWord)
+// transformer('JavaScript is the best', upperFirstWord)
+// transformer('JavaScript is the best', oneWord)
 
-// JS uses callbacks all the time
-const high5 = function() {
-    console.log('👋')
-}
-document.body.addEventListener('click', high5);
+// // JS uses callbacks all the time
+// const high5 = function() {
+//     console.log('👋')
+// }
+// document.body.addEventListener('click', high5);
 
-['Jonas', 'Martha', 'Adam'].forEach(high5)
+// ['Jonas', 'Martha', 'Adam'].forEach(high5)
 
 
 // How Passing Arguments Works: Value vs. Reference
